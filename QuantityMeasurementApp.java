ba40bea13b09e30c9e66902aefd88631f69f7f8f
Chaitanya303212 <chaitanyapotnuru006@gmail.com>
2026-04-22 11:10:22 +0530

    UC4: Add yard and centimeter support using enum

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -2,7 +2,9 @@ public class QuantityMeasurementApp {
 
     enum LengthUnit {
         FEET(1.0),
-        INCH(1.0 / 12.0);
+        INCH(1.0 / 12.0),
+        YARD(3.0),
+        CM(0.393701 / 12.0);
 
         private final double toFeet;
 
@@ -43,17 +45,22 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.YARD);
+        QuantityLength q2 = new QuantityLength(3.0, LengthUnit.FEET);
 
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
-        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARD);
+        QuantityLength q4 = new QuantityLength(36.0, LengthUnit.INCH);
 
-        QuantityLength q5 = new QuantityLength(2.0, LengthUnit.FEET);
+        QuantityLength q5 = new QuantityLength(1.0, LengthUnit.CM);
+        QuantityLength q6 = new QuantityLength(0.393701, LengthUnit.INCH);
 
-        System.out.println("Feet to Inches Equal: " + q1.equals(q2));
-        System.out.println("Inch to Inch Equal: " + q3.equals(q4));
-        System.out.println("Different Value: " + q1.equals(q5));
+        QuantityLength q7 = new QuantityLength(2.0, LengthUnit.YARD);
+        QuantityLength q8 = new QuantityLength(2.0, LengthUnit.YARD);
+
+        System.out.println("Yard to Feet: " + q1.equals(q2));
+        System.out.println("Yard to Inch: " + q3.equals(q4));
+        System.out.println("CM to Inch: " + q5.equals(q6));
+        System.out.println("Same Yard Value: " + q7.equals(q8));
         System.out.println("Null Comparison: " + q1.equals(null));
         System.out.println("Same Reference: " + q1.equals(q1));
     }
